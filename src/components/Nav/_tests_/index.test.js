ffc77362f6d36b92bbba0e89673066732bddd6ca
dmknapp2385 <dmknapp2385@gmail.com>
2022-03-16 11:36:36 -0700

    nav tests

---
+++ src/components/Nav/_tests_/index.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Nav from '..';
+
+afterEach(cleanup);
+
+describe('Nav Component', () => {
+    it('renders',  () => {
+        render(<Nav/>);
+    });
+
+    it('matches snapshot', () => {
+        const { asFragment } = render(<Nav/>);
+        expect(asFragment()).toMatchSnapshot();
+    })
+});
+
+// check if emoji is visible
+describe('emoji is visible', () => {
+    it('inserts emoji into the h2', () => {
+        const { getByLabelText } = render(<Nav/>);
+        expect(getByLabelText('camera')).toHaveTextContent('📸');
+    })
+});
+
+// check if links are visible
+describe('links are visible', () => {
+    it('inserts text into the links', () => {
+        const { getByTestId } = render(<Nav/>);
+
+        expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+        expect(getByTestId('about')).toHaveTextContent('About Me');
+    })
+})
