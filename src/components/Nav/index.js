254f525cbcc87c98e14caac435eb756f97ba2d0d
dmknapp2385 <dmknapp2385@gmail.com>
2022-03-22 16:25:26 -0700

    conditional rendering of form ;

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -6,7 +6,9 @@ function Nav(props) {
     const {
         categories =[],
         setCurrentCategory,
-        currentCategory
+        currentCategory,
+        contactSelected,
+        setContactSelected
     } = props;
 
     useEffect(() => {
@@ -26,21 +28,22 @@ function Nav(props) {
             <ul className="flex-row">
               <li className="mx-2">
                 <a data-testid="about"
-                  href="#about"
+                  href="#about" onClick={() => setContactSelected(false)}
                 >
                   About me
                 </a>
               </li>
-              <li>
-                <span>Contact</span>
+              <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+                <span onClick={() => setContactSelected(true)}>Contact</span>
               </li>
               {categories.map((category) => (
                 <li className={`mx-1 ${
-                    currentCategory.name === category.name && 'navActive'
+                    currentCategory.name === category.name && !contactSelected && 'navActive'
                     }`} key={category.name}>
                   <span
                     onClick={() => {
-                      setCurrentCategory(category)
+                      setCurrentCategory(category);
+                      setContactSelected(false)
                     }}
                   >
                     {capitalizeFirstLetter(category.name)}
